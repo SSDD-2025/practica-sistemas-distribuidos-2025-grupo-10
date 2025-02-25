46d2ff532ebc8c9d6abc5a61bfe50263eacf8478
VeroRam1 <verorama63@gmail.com>
2025-02-25 17:46:25 +0100

    changed mustache files to html and somehow fixed the mainPage problem

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -2,16 +2,11 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
 public class MainController {
     @GetMapping("/mainPage")
     public String showMainPage(){
         //  Llame algun metodo de inicializar usuario
-        return "product";
-    }
-    @GetMapping("/startProducts")
-    public String redirectToProducts(){
-        return "redirect:/products";
+        return "mainPage";
     }
 }
